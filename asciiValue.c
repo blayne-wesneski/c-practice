9c9c806c2d6c656a2544f213fe1cf1d2a9a90fbe
Blayne Wesneski <bwesneskibusiness@gmail.com>
2024-07-22 14:11:18 -0400

    Created asciiValue.c -Takes input of character and prints ASCII value

---
+++ asciiValue.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(){
+
+    char ch;
+
+    printf("input character\n");
+
+    scanf("%c", &ch);
+
+    printf("The ASCII Value of %c is %d\n", ch, ch);
+
+    return 0;
+}
